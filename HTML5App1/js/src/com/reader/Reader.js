1d45365481e37d2a7c857878444ba0264ced34cb
Anoop <anoop26667@gmail.com>
2013-09-22 18:17:10 +0530

    hanges

--- HTML5App1/js/src/com/reader/Reader.js
+++ HTML5App1/js/src/com/reader/Reader.js
@@ -3,7 +3,7 @@ fm.Import("com.reader.hash.RegisterHash");
 fm.Import("jfm.html.LoopScope");
 fm.Include("lib.parser");
 fm.Class("Reader");
-com.reader.Reader = function (me, RegisterHash, SourceController) {
+com.reader.Reader = function (me, RegisterHash) {
     'use strict';
     this.setMe = function (_me) { me = _me };
     Static.main = function () {
